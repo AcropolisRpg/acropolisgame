a7e2746561607708583842f6f8113d6edf6a9636
Ignacio Suarez <ignacio@hristovdevelopment.com>
2022-06-05 20:38:45 -0500

    fix

--- acropolis/src/game/utils/stringHandler.js
+++ acropolis/src/game/utils/stringHandler.js
@@ -1,7 +1,12 @@
+// export const pathHandler = (url) => {
+//   let pathPrefix = '';
+//   if (process.env.ENVI === 'production') {
+//     pathPrefix = '/game';
+//   }
+//   return pathPrefix + url;
+// };
+
 export const pathHandler = (url) => {
-  let pathPrefix = '';
-  if (process.env.ENVI === 'production') {
-    pathPrefix = '/game';
-  }
+  let pathPrefix = '/game';
   return pathPrefix + url;
 };
