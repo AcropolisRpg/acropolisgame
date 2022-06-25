99a260c3ceb9d2e69be24eb6e674ab330d28e546
Ignacio Suarez <ignacio@hristovdevelopment.com>
2022-06-25 13:47:32 -0500

    latest changes;

--- acropolis/src/game/systems/createItemsSystem.ts
+++ acropolis/src/game/systems/createItemsSystem.ts
@@ -15,201 +15,198 @@ export const createItemsSystem = (scene: Phaser.Scene) => {
   let y = 416;
   let counter = 1;
   const inventoryLayout = [];
-
   let layoutX = 288;
   let layoutY = 416;
   let inventoryId = 1;
   let entity;
   let networkEntity;
-  return defineSystem((world) => {
-    // const enterEntities = spriteQueryEnter(world);
-    // for (let i = 0; i < enterEntities.length; i++) {
-    // const id = enterEntities[i];
-    // const entity = getLocalEntityByLocalId(id);
-    // entity.text = {}
 
-    if (window.acropolis.currentPlayerId && !executed) {
-      console.log('con una chingada');
-      entity = window.acropolis.networkSystem.getLocalEntityByNetworkId(
-        window.acropolis.currentPlayerId
-      );
-      networkEntity =
-        window.acropolis.networkSystem.getNetworkEntityByNetworkID(
-          window.acropolis.currentPlayerId
-        );
-      if (entity?.sprites) {
-        executed = true;
-
-        entity.sprites.inventory = scene.add.sprite(400, 370, 'inventory');
-        entity.sprites.backpack = scene.add.sprite(575, 575, 'backpack');
-        for (let y = 0; y < 4; y++) {
-          layoutX = 288;
-          for (let x = 0; x < 8; x++) {
-            inventoryLayout.push({
-              id: inventoryId,
-              position: { x: layoutX, y: layoutY },
-              itemId: ''
-            });
-            layoutX += 32;
-            inventoryId++;
-          }
-          layoutY += 32;
-        }
-        if (networkEntity.items) {
-          //estop ver donde va
-          entity.sprites.items = {};
-
-          console.log('entity.items', entity.items);
-
-          console.log('inventoryLayout', inventoryLayout);
-          if (entity.items) {
-            // Layout 16x64
-            for (const [key, networkItem] of Object.entries<any>(
-              networkEntity.items
-            )) {
-              if (!entity.sprites.items[key]) {
-                const item = {
-                  sprite: scene.add.sprite(x, y, 'icons'),
-                  qty: scene.add.text(x, y, networkItem.quantity)
-                };
-                inventoryLayout.forEach((inventoryItem) => {
-                  if (
-                    inventoryItem.position.x === item.sprite.x &&
-                    inventoryItem.position.y === item.sprite.y
-                  ) {
-                    inventoryItem.itemId = key;
-                  }
-                });
-                console.log('inventoryLayout', inventoryLayout);
-                // .find(inventoryLayout, { 'age': 1, 'active': true });
-                entity.sprites.items[key] = item;
-                // entity.sprites.items[key].text = scene.add.text(288, 416, item);
-                //wood
-                switch (key) {
-                  case 'basicWood':
-                    entity.sprites.items[key].sprite.setFrame(16 * 60 - 2);
-                    break;
-                  case 'basicStone':
-                    entity.sprites.items[key].sprite.setFrame(16 * 54 - 5);
-                    break;
-                  case 'basicHerb':
-                    entity.sprites.items[key].sprite.setFrame(16 * 18);
-                    break;
-                  default:
-                    break;
-                }
-                scene.input.topOnly = false;
-                //stone
-                //herb
-                entity.sprites.items[key].sprite.setInteractive();
-                entity.sprites.items[key].sprite.setData({ id: key });
-                entity.sprites.items[key].sprite.setScrollFactor(0, 0);
-                entity.sprites.items[key].qty.setData({ id: key });
-                entity.sprites.items[key].qty.setInteractive({
-                  draggable: true,
-                  // SETUP hitarea
-                  hitArea: new Phaser.Geom.Rectangle(-16, -16, 32, 32),
-                  //Check hitarea
-                  hitAreaCallback: function (hitArea, x, y) {
-                    return Phaser.Geom.Rectangle.Contains(hitArea, x, y);
-                  }
-                });
-                entity.sprites.items[key].qty.setScrollFactor(0, 0);
-                // const group = scene.add.group(entity.sprites.items[key].sprite, entity.sprites.items[key].qty)
-                scene.input.setDraggable(entity.sprites.items[key].sprite);
-
-                // scene.input.setDraggable(entity.sprites.items[key].qty);
-                x += 32;
-                counter++;
-                if (counter > 8) {
-                  y += 32;
-                  x = 288;
-                }
-              }
-            }
-          }
-          // entity.sprites.items.wood = scene.add.sprite(320, 416, 'icons');
-          // entity.sprites.items.wood.setFrame(3)
-          // entity.sprites.items.wood.setInteractive();
-          // entity.sprites.items.wood.setScrollFactor(0, 0)
-          // scene.input.setDraggable(entity.sprites.items.wood);
-        }
-
-        entity.sprites.backpack.setScrollFactor(0, 0);
-        entity.sprites.backpack.setInteractive();
-        window.acropolis.inventory = {};
-        window.acropolis.inventory.inventoryOpen = true;
-        entity.sprites.inventory.setScrollFactor(0, 0);
-        entity.sprites.inventory.setInteractive();
-        entity.sprites.inventory.setScale(2);
-        entity.sprites.backpack.setName('backpack');
-        // entity.sprites.backpack.setCrop(0, 0, 40, 10);
-        scene.input.on('drag', function (pointer, gameObject, dragX, dragY) {
-          if (dragX >= 288 && dragX <= 512 && dragY >= 416 && dragY <= 512) {
-            gameObject.x = Phaser.Math.Snap.To(dragX, 32);
-            gameObject.y = Phaser.Math.Snap.To(dragY, 32);
-            inventoryLayout.forEach((inventoryItem) => {
-              if (
-                inventoryItem.position.y === gameObject.y &&
-                inventoryItem.position.x === gameObject.x &&
-                inventoryItem.itemId // y tiene item
-              ) {
-                const currentItemInventory = inventoryLayout.find(
-                  (invItem) => invItem.itemId === gameObject.getData('id')
-                );
-                if (currentItemInventory) {
-                  const itemId = currentItemInventory.itemId;
-                  entity.sprites.items[inventoryItem.itemId].sprite.x =
-                    currentItemInventory.position.x;
-                  entity.sprites.items[inventoryItem.itemId].sprite.y =
-                    currentItemInventory.position.y;
-                  entity.sprites.items[inventoryItem.itemId].qty.x =
-                    currentItemInventory.position.x;
-                  entity.sprites.items[inventoryItem.itemId].qty.y =
-                    currentItemInventory.position.y;
-                  currentItemInventory.itemId = inventoryItem.itemId;
-                  inventoryItem.itemId = itemId;
-                }
-              } else if (
-                inventoryItem.position.y === gameObject.y &&
-                inventoryItem.position.x === gameObject.x
-              ) {
-                const currentItemInventory = inventoryLayout.find(
-                  (invItem) => invItem.itemId === gameObject.getData('id')
-                );
-                inventoryItem.itemId = gameObject.getData('id');
-                currentItemInventory.itemId = '';
-                // console.log('Inventory Layout',inventoryLayout)
-              }
-            });
-          }
+  const generateInventoryLayout = () =>{
+    for (let y = 0; y < 4; y++) {
+      layoutX = 288;
+      for (let x = 0; x < 8; x++) {
+        inventoryLayout.push({
+          id: inventoryId,
+          position: { x: layoutX, y: layoutY },
+          itemId: ''
         });
-        scene.input.on(
-          'pointerdown',
-          (
-            pointer: Phaser.Input.Pointer,
-            objectsClicked: Phaser.GameObjects.GameObject[]
-          ) => {
-            if (objectsClicked?.[0]?.name === 'backpack') {
-              window.acropolis.inventory.inventoryOpen =
-                !window.acropolis.inventory.inventoryOpen;
+        layoutX += 32;
+        inventoryId++;
+      }
+      layoutY += 32;
+    }
+  }
 
-              if (entity?.sprites?.items) {
-                for (const [, item] of Object.entries<any>(
-                  entity.sprites.items
-                )) {
-                  item.sprite.setVisible(!item.sprite.visible);
-                  item.qty.setVisible(!item.qty.visible);
-                }
-              }
-              entity.sprites.inventory.setVisible(
-                !entity.sprites.inventory.visible
-              );
-            }
+  const makeItemDraggable = (entity,key) => {
+    entity.sprites.items[key].sprite.setInteractive();
+    entity.sprites.items[key].sprite.setData({ id: key });
+    entity.sprites.items[key].sprite.setScrollFactor(0, 0);
+    entity.sprites.items[key].qty.setData({ id: key });
+    entity.sprites.items[key].qty.setInteractive({
+      draggable: true,
+      // SETUP hitarea
+      hitArea: new Phaser.Geom.Rectangle(-16, -16, 32, 32),
+      //Check hitarea
+      hitAreaCallback: function (hitArea, x, y) {
+        return Phaser.Geom.Rectangle.Contains(hitArea, x, y);
+      }
+    });
+    entity.sprites.items[key].qty.setScrollFactor(0, 0);
+    scene.input.setDraggable(entity.sprites.items[key].sprite);
+  }
+
+  const updateLayoutCounter = () =>{
+    x += 32;
+    counter++;
+    if (counter > 8) {
+      y += 32;
+      x = 288;
+    }
+  }
+
+  const setItemImage = (entity, key) => {
+    switch (key) {
+      case 'basicWood':
+        entity.sprites.items[key].sprite.setFrame(16 * 60 - 2);
+        break;
+      case 'basicStone':
+        entity.sprites.items[key].sprite.setFrame(16 * 54 - 5);
+        break;
+      case 'basicHerb':
+        entity.sprites.items[key].sprite.setFrame(16 * 18);
+        break;
+      default:
+        break;
+    }
+  }
+
+  return defineSystem((world) => {
+    if (!window?.acropolis?.currentPlayerId || executed) {
+      return;
+    }
+    entity = window.acropolis.networkSystem.getLocalEntityByNetworkId(
+      window.acropolis.currentPlayerId
+    );
+    networkEntity = window.acropolis.networkSystem.getNetworkEntityByNetworkID(
+      window.acropolis.currentPlayerId
+    );
+    if (!entity?.sprites) {
+      return;
+    }
+    executed = true;
+
+    entity.sprites.inventory = scene.add.sprite(400, 370, 'inventory');
+    entity.sprites.backpack = scene.add.sprite(575, 575, 'backpack');
+    scene.input.topOnly = false;
+
+    generateInventoryLayout()
+
+    if (networkEntity.items) {
+      //estop ver donde va
+      entity.sprites.items = {};
+      if (entity.items) {
+        // Layout 16x64
+        for (const [key, networkItem] of Object.entries<any>(
+          networkEntity.items
+        )) {
+          if (entity.sprites.items[key]) {
+            continue;
           }
-        );
+          const item = {
+            sprite: scene.add.sprite(x, y, 'icons'),
+            qty: scene.add.text(x, y, networkItem.quantity)
+          };
+          inventoryLayout.forEach((inventoryItem) => {
+            if (
+              inventoryItem.position.x === item.sprite.x &&
+              inventoryItem.position.y === item.sprite.y
+            ) {
+              inventoryItem.itemId = key;
+            }
+          });
+          entity.sprites.items[key] = item;
+ 
+          setItemImage(entity, key)
+          //stone
+          //herb
+
+          // Make items drraggable
+          makeItemDraggable(key, entity)
+          updateLayoutCounter()
+        }
       }
     }
+
+    entity.sprites.backpack.setScrollFactor(0, 0);
+    entity.sprites.backpack.setInteractive();
+    window.acropolis.inventory = {};
+    window.acropolis.inventory.inventoryOpen = true;
+    entity.sprites.inventory.setScrollFactor(0, 0);
+    entity.sprites.inventory.setInteractive();
+    entity.sprites.inventory.setScale(2);
+    entity.sprites.backpack.setName('backpack');
+    scene.input.on('drag', function (pointer, gameObject, dragX, dragY) {
+      if (dragX >= 288 && dragX <= 512 && dragY >= 416 && dragY <= 512) {
+        gameObject.x = Phaser.Math.Snap.To(dragX, 32);
+        gameObject.y = Phaser.Math.Snap.To(dragY, 32);
+        inventoryLayout.forEach((inventoryItem) => {
+          if (
+            inventoryItem.position.y === gameObject.y &&
+            inventoryItem.position.x === gameObject.x &&
+            inventoryItem.itemId // y tiene item
+          ) {
+            const currentItemInventory = inventoryLayout.find(
+              (invItem) => invItem.itemId === gameObject.getData('id')
+            );
+            if (currentItemInventory) {
+              const itemId = currentItemInventory.itemId;
+              entity.sprites.items[inventoryItem.itemId].sprite.x =
+                currentItemInventory.position.x;
+              entity.sprites.items[inventoryItem.itemId].sprite.y =
+                currentItemInventory.position.y;
+              entity.sprites.items[inventoryItem.itemId].qty.x =
+                currentItemInventory.position.x;
+              entity.sprites.items[inventoryItem.itemId].qty.y =
+                currentItemInventory.position.y;
+              currentItemInventory.itemId = inventoryItem.itemId;
+              inventoryItem.itemId = itemId;
+            }
+          } else if (
+            inventoryItem.position.y === gameObject.y &&
+            inventoryItem.position.x === gameObject.x
+          ) {
+            const currentItemInventory = inventoryLayout.find(
+              (invItem) => invItem.itemId === gameObject.getData('id')
+            );
+            inventoryItem.itemId = gameObject.getData('id');
+            currentItemInventory.itemId = '';
+            // console.log('Inventory Layout',inventoryLayout)
+          }
+        });
+      }
+    });
+    scene.input.on(
+      'pointerdown',
+      (
+        pointer: Phaser.Input.Pointer,
+        objectsClicked: Phaser.GameObjects.GameObject[]
+      ) => {
+        if (objectsClicked?.[0]?.name === 'backpack') {
+          window.acropolis.inventory.inventoryOpen =
+            !window.acropolis.inventory.inventoryOpen;
+
+          if (entity?.sprites?.items) {
+            for (const [, item] of Object.entries<any>(entity.sprites.items)) {
+              item.sprite.setVisible(!item.sprite.visible);
+              item.qty.setVisible(!item.qty.visible);
+            }
+          }
+          entity.sprites.inventory.setVisible(
+            !entity.sprites.inventory.visible
+          );
+        }
+      }
+    );
 
     // TODO actualizar los estupidos valores numericos de la base de datos porque claro que si!
     // TODO revidsar porque se rompe el sistema de ordenamiento de items.
@@ -236,19 +233,7 @@ export const createItemsSystem = (scene: Phaser.Scene) => {
     //       };
     //       item.sprite.setVisible(!item.sprite.visible);
     //       item.qty.setVisible(!item.qty.visible);
-    //       inventoryLayout.forEach((inventoryItem) => {
-    //         if (
-    //           inventoryItem.position.x === item.sprite.x &&
-    //           inventoryItem.position.y === item.sprite.y
-    //         ) {
-    //           inventoryItem.itemId = key;
-    //         }
-    //       });
-    //       // console.log('inventoryLayout', inventoryLayout);
-    //       // .find(inventoryLayout, { 'age': 1, 'active': true });
     //       entity.sprites.items[key] = item;
-    //       // entity.sprites.items[key].text = scene.add.text(288, 416, item);
-    //       //wood
     //       switch (key) {
     //         case 'basicWood':
     //           entity.sprites.items[key].sprite.setFrame(16 * 60 - 2);
@@ -263,8 +248,6 @@ export const createItemsSystem = (scene: Phaser.Scene) => {
     //           break;
     //       }
     //       scene.input.topOnly = false;
-    //       //stone
-    //       //herb
     //       entity.sprites.items[key].sprite.setInteractive();
     //       entity.sprites.items[key].sprite.setData({ id: key });
     //       entity.sprites.items[key].sprite.setScrollFactor(0, 0);
@@ -278,12 +261,8 @@ export const createItemsSystem = (scene: Phaser.Scene) => {
     //           return Phaser.Geom.Rectangle.Contains(hitArea, x, y);
     //         }
     //       });
-
     //       entity.sprites.items[key].qty.setScrollFactor(0, 0);
-    //       // const group = scene.add.group(entity.sprites.items[key].sprite, entity.sprites.items[key].qty)
     //       scene.input.setDraggable(entity.sprites.items[key].sprite);
-
-    //       // scene.input.setDraggable(entity.sprites.items[key].qty);
     //       x += 32;
     //       counter++;
     //       if (counter > 8) {
@@ -292,7 +271,6 @@ export const createItemsSystem = (scene: Phaser.Scene) => {
     //       }
     //     }
     //   }
-    // }
     // }
 
     // if (window.acropolis.currentPlayerId) {
