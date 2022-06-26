ae7f13e0615769db93198be83595cdf4715badbe
Ignacio Suarez <ignacio@hristovdevelopment.com>
2022-06-26 12:23:30 -0500

    change public path

--- acropolis/vue.config.js
+++ acropolis/vue.config.js
@@ -1,8 +1,9 @@
 const NodePolyfillPlugin = require("node-polyfill-webpack-plugin")
 module.exports = {
-  publicPath: process.env.ENVI === 'production'
-    ? '/game'
-    : '/',
+  publicPath:  '/game',
+  // publicPath: process.env.ENVI === 'production'
+  //   ? '/game'
+  //   : '/',
     transpileDependencies: true,
     configureWebpack: {
       plugins: [
